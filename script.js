05a1ac6114fabd9f9b1713e3c903788550d35932
KARTHIK PILLI <117244354+KKBAE143@users.noreply.github.com>
2023-11-17 07:29:41 +0000

    added files index.html style.css script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x - 250 +"px"
+    blur.style.top = dets.y - 250 +"px"
+})
+document.addEventListener("mousemove",function(dets){
+   
+})
+
+
+gsap.to("#nav",{
+    backgroundColor :"#000",
+    height:"100px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        //markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1,
+    }
+
+})
+
+gsap.to("#main",{
+    backgroundColor : "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+      //  markers:true
+      start:"top -25%",
+      end:"top -70%",
+      scrub:2
+    }
+})
+
